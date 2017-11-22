bfdf9368d74212e2eb3ad5baf105ed1b9cfde248
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2017-11-22 13:31:41 -0500

    revert profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,102 +6,13 @@ module.exports = {
   registerRouter() {
     const router = express.Router();
 
-    router.get('/', Redirect.ifNotLoggedIn(), Redirect.ifNotCustomer('/profile/d/'),Redirect.ifNotDeveloper('/profile/c/'),this.index);
-    router.get('/c/', Redirect.ifNotLoggedIn(), Redirect.ifNotApproved(), Redirect.ifNoSetUp(),
-                      Redirect.ifNotCustomer('/profile/d/'), this.indexCustomer);
-    router.get('/d/', Redirect.ifNotLoggedIn(), Redirect.ifNotApproved(), Redirect.ifNoSetUp(),
-                      Redirect.ifNotDeveloper(), this.indexDeveloper);
+    router.get('/', Redirect.ifNotLoggedIn(), Redirect.ifNotApproved(), Redirect.ifNoSetUp(), this.index);
 
     return router;
   },
-
-  index(req,res){
-
-  },
-
-  indexCustomer(req, res) {
-    models.Post.findAll({
-      where:{
-        userId:req.user.id,
-      },
-      include: [{
-        model: models.Bid,
-        where:{
-          attributes:[
-          [models.sequelize.fn('min', models.sequelize.col('price')),'price'],
-        ]
-        },
-        /*
-
-        order: [ [ 'price', 'DESC' ]],
-        limit: 1,
-        
-        
-        */
-      }]
-    }).then((allPosts) => {
-       req.user.getProfile()
-       .then((profile) =>{
-        console.log(allPosts);
-      res.render('profile/customer', { user: req.user, profile: profile, allPosts: allPosts,  success: req.flash('success') });
+  index(req, res) {req.user.getProfile()
+    .then((p) => {
+      res.render('profile', { user: req.user, profile: p, success: req.flash('success') });
     });
-     });
   },
-
-    indexDeveloper(req, res) {
-    models.Post.findAll({
-      group: ['post.id'],
-      /*
-      include: [{
-        model: models.Bid,
-        where: {
-          userId: req.user.id,
-        },
-        attributes:[
-          [models.sequelize.fn('min', models.sequelize.col('price')),'price'],
-        ]
-      }],*/
-    }).then((allPosts) => {
-       req.user.getProfile()
-       .then((profile) =>{
-        console.log(allPosts);
-      res.render('profile/developer', { user: req.user, profile: profile, allPosts: allPosts,  success: req.flash('success') });
-    });
-     });
-  },
-
 };
-
-/*module.exports = {
-  registerRouter() {
-    const router = express.Router();
-
-    router.get('/c/', Redirect.ifNotLoggedIn(), Redirect.ifNotApproved(), Redirect.ifNoSetUp(), 
-                      Redirect.ifNotCustomer('/profile/d/'), this.indexCustomer);
-    router.get('/d/', Redirect.ifNotLoggedIn(), Redirect.ifNotApproved(), Redirect.ifNoSetUp(),
-                      Redirect.ifNotDeveloper('profile/c/'), this.indexDeveloper);
-  },
-
-  indexCustomer(req, res) {
-    models.Post.findAll({
-      where:{
-        userId:req.user.id,
-      },
-            include: [{
-        model: models.Bid,
-        where:{
-          attributes:[
-          [models.sequelize.fn('min', models.sequelize.col('price')),'price'],
-          ],
-        },
-        }]
-
-    }).then((allPosts) => {
-      res.render('profile', { user: req.user, profile: req.user.getProfile(), success: req.flash('success') });
-    });
-  },
-
-  indexDeveloper(req, res){
-    res.render('profile');
-  },
-};*/
--- middlewares/viewHelpers.js
+++ middlewares/viewHelpers.js
@@ -22,3 +22,4 @@ helpers.register = () => {
 };
 
 module.exports = helpers;
+
